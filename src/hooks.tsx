7786b6ca27c6d6b305bbb5f79159d0c120a0c9fe
Gabe Abud <gabriel.jabud@gmail.com>
2020-07-26 09:35:29 -0700

    change so field order shouldn't matter in data and add test

--- src/hooks.tsx
+++ src/hooks.tsx
@@ -21,7 +21,18 @@ export type UseTableType = (
 export const useTable: UseTableType = (columns, data) => {
   const [tableCols] = useState(columns);
   const tableData: RowType[] = useMemo(() => {
-    return data.map(row => {
+    const sortedData = data.map((row: any) => {
+      const newRow: any = {};
+      columns.forEach(column => {
+        if (!(column.name in row)) {
+          throw new Error(`Invalid row data, ${column.name} not found`);
+        }
+        newRow[column.name] = row[column.name];
+      });
+      return newRow;
+    });
+
+    return sortedData.map(row => {
       return {
         cells: Object.entries(row).map(([column, value]) => {
           return {
--- src/test/hooks.test.tsx
+++ src/test/hooks.test.tsx
@@ -1,31 +1,37 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import { useTable } from '../hooks';
+import { useTable, ColumnType } from '../hooks';
 
 const columns = [
   {
-    name: 'first_name',
+    name: 'firstName',
     label: 'First Name',
   },
   {
-    name: 'last_name',
+    name: 'lastName',
     label: 'Last Name',
   },
 ];
 
 const data = [
   {
-    first_name: 'Frodo',
-    last_name: 'Baggins',
+    firstName: 'Frodo',
+    lastName: 'Baggins',
   },
   {
-    first_name: 'Samwise',
-    last_name: 'Gamgee',
+    firstName: 'Samwise',
+    lastName: 'Gamgee',
   },
 ];
 
-const App = () => {
+const Table = ({
+  columns,
+  data,
+}: {
+  columns: ColumnType[];
+  data: Object[];
+}) => {
   const { headers, rows } = useTable(columns, data);
 
   return (
@@ -51,10 +57,28 @@ const App = () => {
 };
 
 test('Should render a basic table', () => {
-  const rtl = render(<App />);
+  const rtl = render(<Table columns={columns} data={data} />);
 
   expect(rtl.getByText('Frodo')).toBeInTheDocument();
   expect(rtl.getByText('Baggins')).toBeInTheDocument();
   expect(rtl.getByText('Samwise')).toBeInTheDocument();
   expect(rtl.getByText('Gamgee')).toBeInTheDocument();
+});
+
+const reverseData = [
+  {
+    lastName: 'Baggins',
+    firstName: 'Frodo',
+  },
+  {
+    lastName: 'Gamgee',
+    firstName: 'Samwise',
+  },
+];
+
+test('Should be equal regardless of field order in data', () => {
+  const normalTl = render(<Table columns={columns} data={data} />);
+  const reverseTl = render(<Table columns={columns} data={reverseData} />);
+
+  expect(normalTl.asFragment()).toEqual(reverseTl.asFragment());
 });
